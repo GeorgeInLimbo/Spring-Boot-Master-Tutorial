654c69afc573be667543ae31ef5677120099ebb1
George Clam <gclam77@gmail.com>
2023-04-03 16:08:38 -0600

    Building APIs with Spring MVC

--- src/main/java/com/smt/georgeclam/springbootmastertutorial/customer/Customer.java
+++ src/main/java/com/smt/georgeclam/springbootmastertutorial/customer/Customer.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Customer {
     private final Long id;
     private final String name;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private final String password;
 
     public Customer(Long id,
