f011c17f0ec067800d87eb2667271321935eafc5
George Clam <gclam77@gmail.com>
2023-04-08 09:37:09 -0600

    Open Feign REST Client

--- src/main/java/com/smt/georgeclam/springbootmastertutorial/Entry.java
+++ src/main/java/com/smt/georgeclam/springbootmastertutorial/Entry.java
@@ -2,8 +2,10 @@ package com.smt.georgeclam.springbootmastertutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Entry {
 	public static void main(String[] args) {
 		SpringApplication.run(Entry.class, args);
